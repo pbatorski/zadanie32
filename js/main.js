f00293062d8710f46ba6de473c9f3a07630dd0d8
przemour@gmail.com <przemour@gmail.com>
2018-09-15 11:44:59 +0200

    zadanie 32 wykonane

---
+++ js/main.js
@@ -0,0 +1,20 @@
+var num1 = 2,
+    num2 = 5;
+
+function larger () {
+    if (num1 > num2) {
+        console.log ("num1 jest większe od num2");
+    }
+        else
+        console.log ("num2 jest większe od num1");   
+            
+}
+
+
+/*
+var liczbaPierwsza = 8,
+    liczbaDruga = 12;
+
+function suma () {
+    return liczbaPierwsza + liczbaDruga;
+}*/
